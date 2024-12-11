4dbab3fa69303a4e97f4a2f54ab90b4dff1cbb48
azuna8288 <kongrui.1209@bytedance.com>
2024-12-11 09:55:11 +0800

    update

---
+++ upload_model.py
@@ -0,0 +1,35 @@
+from modelscope.hub.constants import ModelVisibility
+from modelscope.hub.api import HubApi
+import os
+
+YOUR_ACCESS_TOKEN = '0904907c-fc44-4147-af35-fee543d9f27a'
+api = HubApi()
+api.login(YOUR_ACCESS_TOKEN)
+
+
+if os.environ.get("MODEL_NAME", None) == None:
+    print('no such ckpt, please check')
+    exit(0)
+
+
+username = 'monster119120'
+version = 0
+create_sucess = False
+while not create_sucess:
+    try:
+        model_name = f'Molmo7b_CN_v{version}'
+        model_id=f'{username}/{model_name}'
+
+        api.create_model(
+            model_id,
+            visibility=ModelVisibility.PRIVATE,
+        )
+        create_sucess = True
+    except:
+        print('try raise version')
+        version += 1
+
+api.push_model(
+    model_id=model_id, 
+    model_dir=os.environ.get("MODEL_NAME", None)
+)
