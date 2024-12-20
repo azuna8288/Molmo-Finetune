1a7a5e015a754eb154e43258853a783097003576
azuna8288 <kongrui.1209@bytedance.com>
2024-12-20 20:54:19 +0800

    update

--- evaluation/eval_main.py
+++ evaluation/eval_main.py
@@ -56,6 +56,7 @@ all_data = pd.read_parquet(args.parquet_file_path)
 
 success_count = 0
 total_count = 0
+error_count = 0
 for idx, single_data in tqdm(all_data.iterrows()):
     img_path = os.path.join(args.img_folder_path, single_data['view']+'.png')
     question = f"""You are going to refer an element on the UI interface of the image, following the instruction.\nPlease point out the element you want to refer to complete the instruction.\n\nYour instruction:\nwhere is 位于{single_data['abs_pos']}，并且{single_data['rel_pos']}的{single_data['description']}?\nNote:\n1. Remember always to give a point to refer an element!\n2. Your response should be like this: ' <point x="?" y="?" alt="?">?</point>\ '\nBegin!"""
@@ -77,12 +78,16 @@ for idx, single_data in tqdm(all_data.iterrows()):
 
     generated_tokens = output[0,inputs['input_ids'].size(1):]
     generated_answer = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
-    point = parse_point(generated_answer)
+    
+    try:
+        point = parse_point(generated_answer)
 
-    bbox = single_data['bbox_x0'], single_data['bbox_y0'], single_data['bbox_x1'], single_data['bbox_y1']
-    bbox = scale_bbox(bbox, img.size)
+        bbox = single_data['bbox_x0'], single_data['bbox_y0'], single_data['bbox_x1'], single_data['bbox_y1']
+        bbox = scale_bbox(bbox, img.size)
 
-    if is_in_rectangle(bbox, point):
-        success_count += 1
+        if is_in_rectangle(bbox, point):
+            success_count += 1
+    except:
+        error_count += 1
     total_count += 1
-print(f"ACC: {success_count/total_count:.2f}")
+print(f"ACC: {success_count/total_count:.2f}, error_count: {error_count}")
