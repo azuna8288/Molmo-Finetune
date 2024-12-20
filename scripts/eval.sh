4d1b2068cde9ed77b291e5e24d4c560e96442193
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-20 11:11:28 +0800

    fix

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,4 +1,4 @@
 python3 evaluation/eval_main.py \
     --model_path Molmo7b_CN_v11 \
-    --parquet_file_path  data/chunk01_1000.parquet \
+    --parquet_file_path  data/ground_test/chunk01_1000.parquet \
     --img_folder_path data/images
