be89819125dac079bce141d66a75c0225e8ceecc
kongrui.1209 <kongrui.1209@bytedance.com>
2024-12-20 11:11:05 +0800

    finish eval

--- evaluation/eval_main.py
+++ evaluation/eval_main.py
@@ -3,15 +3,40 @@ from PIL import Image
 import json
 import os
 import argparse
+import pandas as pd
+from tqdm import tqdm
 
 parser = argparse.ArgumentParser(description="test")
-
 parser.add_argument('--model_path', default="Molmo-7B-D-0924", type=str, help='model path')
-parser.add_argument('--json_file_path', default="data/chunk01_enhanced.json", type=str, help='eval data path')
+parser.add_argument('--parquet_file_path', default="data/chunk01.parquet", type=str, help='eval data path')
 parser.add_argument('--img_folder_path', default="data/images", type=str, help='img path')
-
 args = parser.parse_args()
 
+def scale_bbox(bbox, img_size):
+    width, height = img_size
+    x1, y1, x2, y2 = bbox
+    x1 = x1/width * 100
+    y1 = y1/height * 100
+    x2 = x2/width * 100
+    y2 = y2/height * 100
+    return x1, y1, x2, y2
+
+def is_in_rectangle(box, point):
+    x, y = point
+    x1, y1, x2, y2 = box
+    return (x1 <= x <= x2 and y1 <= y <= y2)
+
+def parse_point(point_str):
+    """
+    point_str will be ' <point x="49.8" y="69.8"></point> '
+    """
+    x_start = point_str.find('x="') + 3
+    x_end = point_str.find('"', x_start)
+    y_start = point_str.find('y="') + 3
+    y_end = point_str.find('"', y_start)
+    x = float(point_str[x_start:x_end])
+    y = float(point_str[y_start:y_end])
+    return x, y
 
 processor = AutoProcessor.from_pretrained(
     args.model_path,
@@ -27,16 +52,17 @@ model = AutoModelForCausalLM.from_pretrained(
     device_map='auto'
 )
 
-with open(args.json_file_path, 'r') as f:
-    all_data = json.load(f)
+all_data = pd.read_parquet(args.parquet_file_path)
 
-for single_data in all_data:
-    img_path = os.path.join(args.img_folder_path, single_data['image'])
-    question = single_data['conversations'][0]['value']
-    gold_answer = single_data['conversations'][1]['value']
+success_count = 0
+total_count = 0
+for idx, single_data in tqdm(all_data.iterrows()):
+    img_path = os.path.join(args.img_folder_path, single_data['view']+'.png')
+    question = f"""You are going to refer an element on the UI interface of the image, following the instruction.\nPlease point out the element you want to refer to complete the instruction.\n\nYour instruction:\nwhere is 位于{single_data['abs_pos']}，并且{single_data['rel_pos']}的{single_data['description']}?\nNote:\n1. Remember always to give a point to refer an element!\n2. Your response should be like this: ' <point x="?" y="?" alt="?">?</point>\ '\nBegin!"""
     
+    img = Image.open(img_path)
     inputs = processor.process(
-        images=[Image.open(img_path)],
+        images=[img],
         text=question
     )
 
@@ -51,7 +77,12 @@ for single_data in all_data:
 
     generated_tokens = output[0,inputs['input_ids'].size(1):]
     generated_answer = processor.tokenizer.decode(generated_tokens, skip_special_tokens=True)
+    point = parse_point(generated_answer)
 
-    print(question, generated_answer, gold_answer)
-    
-    # x = input()
+    bbox = single_data['bbox_x0'], single_data['bbox_y0'], single_data['bbox_x1'], single_data['bbox_y1']
+    bbox = scale_bbox(bbox, img.size)
+
+    if is_in_rectangle(bbox, point):
+        success_count += 1
+    total_count += 1
+print(f"ACC: {success_count/total_count:.2f}")
---
+++ scripts/eval.sh
@@ -0,0 +1,4 @@
+python3 evaluation/eval_main.py \
+    --model_path Molmo7b_CN_v11 \
+    --parquet_file_path  data/chunk01_1000.parquet \
+    --img_folder_path data/images
